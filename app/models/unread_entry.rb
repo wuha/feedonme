0291027c3e59ba73be6ccdb62f7fb65815221470
wuha <simongotz@Simons-MacBook-Air.local>
2013-11-09 11:27:58 +0100

    Remove Try/Catch

--- app/models/unread_entry.rb
+++ app/models/unread_entry.rb
@@ -8,15 +8,12 @@ class UnreadEntry < ActiveRecord::Base
 		User.all.each do |user|
 			user.subscriptions.each do |subscription|
 				Entry.where("feed_id=?",subscription.feed_id).each do |entry|	
-					begin
-						if UnreadEntry.where("entry_id=? and feed_id=? and user_id=?",entry.id,subscription.feed_id,user.id).first
+					if UnreadEntry.where("entry_id=? and feed_id=? and user_id=?",entry.id,subscription.feed_id,user.id).first
+					else
+						if ReadEntry.where("entry_id=? and user_id=?",entry.id,user.id).first
 						else
-							if ReadEntry.where("entry_id=? and user_id=?",entry.id,user.id).first
-							else
-								create(entry_id: entry.id, feed_id: subscription.feed_id, user_id: user.id, date: entry.date )
-							end
+							create(entry_id: entry.id, feed_id: subscription.feed_id, user_id: user.id, date: entry.date )
 						end
-					rescue
 					end
 				end
 			end
